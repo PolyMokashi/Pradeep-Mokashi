a4fd0751334793dcc2ac5d2ead9ea00db7bda513
Pradeep Dilip Mokashi <pradeepmokashi9@gmail.com>
2024-06-12 12:49:11 +0530

    adfa

--- src/pages/Projects/ProjectPage.js
+++ src/pages/Projects/ProjectPage.js
@@ -1,24 +1,24 @@
-/* eslint-disable array-callback-return */
 import { Box, Stack, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import CustomCard from '../../commom-components/CustomCard'
 import ProjectInfo from '../../Constants/ProjectInfo'
 import CommonDrawer from '../../commom-components/CommonDrawer'
 
-const ProjectPage = ({mode}) => {
-    const [selectedCard, setSelectedCard] = useState(null)
-    const [open, setOpen]= useState(false);
+const ProjectPage = ({ mode }) => {
+    const [selectedCard, setSelectedCard] = useState(null);
+    const [open, setOpen] = useState(false);
 
-    const toggleDrawer=()=>{
+    const toggleDrawer = () => {
         setOpen(false);
-    };
+    }
 
-    const handleCardClick=(data)=>{
+    const handleCardClick = (data) => {
         setSelectedCard(data);
         setOpen(true);
-    }
+    };
+
     return (
-        <div>
+        <>
             <Box
                 sx={{
                     display: 'flex',
@@ -37,27 +37,23 @@ const ProjectPage = ({mode}) => {
                 sx={{ display: 'flex', justifyContent: 'center' }}
                 useFlexGap
             >
-                {ProjectInfo.map((data) => {
-                    if (data.Title) {
-                        return (
-                            <div onClick={() => handleCardClick(data)}>
-                                <CustomCard
-                                    key={data.Title}
-                                    project_image={data.project_image}
-                                    AvatarIcon={data.AvatarIcon}
-                                    src={data.src}
-                                    ProjectImg={data.ProjectImg}
-                                    Project_link={data.Project_link}
-                                    Title={data.Title}
-                                    SubHeader={data.SubHeader}
-                                    TechStack={data.TechStack}
-                                    TechIcon={data.TechIcon}
-                                    mode={mode}
-                                />
-                            </div>
-                        )
-                    }
-                })}
+                {ProjectInfo.map((data) => (
+                    <div onClick={() => handleCardClick(data)}>
+                        <CustomCard
+                            key={data.Title}
+                            project_image={data.project_image}
+                            AvatarIcon={data.AvatarIcon}
+                            src={data.src}
+                            ProjectImg={data.ProjectImg}
+                            Project_link={data.Project_link}
+                            Title={data.Title}
+                            SubHeader={data.SubHeader}
+                            TechStack={data.TechStack}
+                            TechIcon={data.TechIcon}
+                            mode={mode}
+                        />
+                    </div>
+                ))}
             </Stack>
             {selectedCard && (
                 <CommonDrawer
@@ -74,7 +70,7 @@ const ProjectPage = ({mode}) => {
                     mode={mode}
                 />
             )}
-        </div>
+        </>
     )
 }
 
