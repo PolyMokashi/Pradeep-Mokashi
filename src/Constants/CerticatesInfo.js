d14179319f89e45a77aa54466ca516fe3bef97b2
Pradeep Dilip Mokashi <94032066+PolyMokashi@users.noreply.github.com>
2025-02-04 22:42:14 +0530

    Update CerticatesInfo.js

--- src/Constants/CerticatesInfo.js
+++ src/Constants/CerticatesInfo.js
@@ -7,7 +7,7 @@ import Udemylogo from '../Assets/icons/Udemy.svg'
 import UdemyImg from '../Assets/Images/Image Processing Udemy_pages-to-jpg-0001.jpg'
 import UdemyImg1 from '../Assets/Images/udemyImg.jpeg'
 import Dyplogo from '../Assets/icons/d_y_patil_college_of_engineering__technology_kasaba_bawada_logo.jpg'
-import DypImage from '../Assets/Images/ quiz_competition_cert.jpeg''
+import DypImage from '../Assets/Images/quiz_competition_cert.jpeg''
 
 const CertificatesInfo = [
     {
