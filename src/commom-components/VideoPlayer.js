f98696937af11feb8e101a0c88a406b87e928749
Pradeep Dilip Mokashi <pradeepmokashi9@gmail.com>
2025-10-28 01:47:20 +0100

    poster added

--- src/commom-components/VideoPlayer.js
+++ src/commom-components/VideoPlayer.js
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 
 const VideoPlayer = ({ src }) => {
+    const posterUrl = src.replace(/\.[^/.]+$/, '.jpg')
 
-    return <ReactPlayer url={src} controls width="100%" height="100%" />
+    return (
+        <ReactPlayer
+            url={src}
+            controls
+            width="100%"
+            height="100%"
+            light={posterUrl}
+        />
+    )
 }
 
 export default VideoPlayer
